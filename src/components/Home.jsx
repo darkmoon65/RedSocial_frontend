f942e6b338312ab3770ed8adc3c6953feb3bfb63
cesar Santillana <icetom6@hotmail.com>
2023-11-17 00:28:28 -0500

    Maquetando Home, columnas y diseño base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,53 @@ import Header from '../components/Header';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{height: '100vh', backgroundColor: '#f4f4f9'}}>
             <Header/>
-            Hola este es el home ... 
+            <div className='container-fluid'>
+                <div className='row ms-4 me-4'>
+                    <div className='col-3'>
+                        <div className='card' style={{height: '80vh'}}>
+                            <div className='card-body'>
+                                Cesar Santillana
+                                <ul>
+                                    <li>Inicio</li>
+                                    <li>Mensajes</li>
+                                    <li>Perfil</li>
+                                    <li>Grupos</li>
+                                    <li>Amigos</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='card p-4 m-3'>
+                            <div className='card-body'>
+                                POSTS 1
+                            </div>
+                        </div>
+                        <div className='card p-4 m-3'>
+                            <div className='card-body'>
+                                POSTS 1
+                            </div>
+                        </div>
+                        <div className='card p-4 m-3'>
+                            <div className='card-body'>
+                                POSTS 1
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-3'>
+                        <div className='card' style={{height: '80vh'}}>
+                            <div className='card-header'>
+                                Contactos
+                            </div>
+                            <div className='card-body'>
+                                Contactos 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
